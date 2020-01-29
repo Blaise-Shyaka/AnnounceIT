1602ea0232f561ce1820a8d070a9d3a629e7779a
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-01-30 00:37:10 +0200

    [finishes #170842257] View announcements of specific state API endpoint

--- server/routes/router.js
+++ server/routes/router.js
@@ -4,6 +4,7 @@ import signinRouter from '../controllers/signin';
 import createAnnouncementRouter from '../controllers/create-announcement';
 import updateAnnouncementRouter from '../controllers/update-announcement';
 import changeStatusRouter from '../controllers/change-status';
+import allSpecificStatusAnnouncementsRouter from '../controllers/all-specific-status-announcements';
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.use('/api/v1/', signinRouter);
 router.use('/api/v1/', createAnnouncementRouter);
 router.use('/api/v1/', updateAnnouncementRouter);
 router.use('/api/v1/', changeStatusRouter);
+router.use('/api/v1/', allSpecificStatusAnnouncementsRouter);
 
 export default router;
